f806f47b2b61cb79bb9cea49adf420122fadfce6
AbdullhaSajid18 <abdullahsajid1098@Gmail.com>
2023-08-18 22:14:26 +0500

    again Updated

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,6 +1,9 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.2.0/firebase-app.js";
-
-import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, onAuthStateChanged, signOut } from "https://www.gstatic.com/firebasejs/10.2.0/firebase-app.js";
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.23.0/firebase-app.js'
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, onAuthStateChanged, signOut } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-auth.js";
+import {
+    getFirestore, collection, addDoc, setDoc, doc, query, where, getDocs, getDoc, updateDoc,
+} from "https://www.gstatic.com/firebasejs/9.23.0/firebase-firestore.js";
+import { getStorage,ref, getDownloadURL, uploadBytesResumable } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-storage.js";
 
 const firebaseConfig = {
     apiKey: "AIzaSyBrhHAyR4l-zyAkuOD2xAIvqg3iemcJLx8",
@@ -16,14 +19,27 @@ const auth = getAuth(app);
 const db = getFirestore(app);
 const storage = getStorage(app);
 
-export{
-    app,
+export {
     auth,
+    app,
     db,
     storage,
+    getFirestore,
+    collection,
+    addDoc,
+    setDoc,
+    doc,
+    getDoc,
+    getAuth,
     createUserWithEmailAndPassword,
+    query,
+    where,
+    getDocs,
     signInWithEmailAndPassword,
     onAuthStateChanged,
-    signOut
-
-}
+    signOut,
+    ref,
+    getDownloadURL,
+    updateDoc,
+    uploadBytesResumable
+};
