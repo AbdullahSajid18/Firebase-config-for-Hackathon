79e8200048f74e3ea29d56ed9513ce93e239fd37
AbdullhaSajid18 <abdullahsajid1098@Gmail.com>
2023-08-18 22:00:43 +0500

    Hackathon Boiler Plate

---
+++ app.js
@@ -0,0 +1,83 @@
+import {auth, app, db, createUserWithEmailAndPassword, signInWithEmailAndPassword, doc, setDoc} from './firebaseConfig.js'
+
+let loginEmail = document.getElementById('loginEmail');
+let loginPassword = document.getElementById('loginPassword');
+
+// buttons
+let loginBtn = document.getElementById('loginBtn');
+
+
+
+function loginHandler() {
+
+signInWithEmailAndPassword(auth, loginEmail.value, loginPassword.value)
+  .then((userCredential) => {
+    // Signed in 
+    const user = userCredential.user;
+    if(user){
+        window.location.href ='./dashboard/index.html';
+    }
+    alert("user logged in")
+  })
+  .catch((error) => {
+      const errorCode = error.code;
+    const errorMessage = error.message;
+    // console.log(errorMessage)
+    alert("user not found")
+  });
+};
+
+loginBtn.addEventListener('click',loginHandler);
+
+// signup
+
+let firstName = document.getElementById('firstName');
+let lastName = document.getElementById('lastName');
+let signEmail = document.getElementById('signEmail');
+let signPassword = document.getElementById('signPassword');
+let phoneNumber = document.getElementById('phoneNumber');
+let dateTime = document.getElementById('dateTime');
+
+// buttons
+let signupBtn = document.getElementById('signupBtn');
+
+async function signUpnHandler() {
+    try {
+    const response =  await createUserWithEmailAndPassword(auth, signEmail.value, signPassword.value)
+
+   alert("user registered")
+   
+   if(response.user){
+        adddata(response.user.uid)
+
+
+}
+  }
+ catch (error) {
+    const errorMessage = error.message;
+    console.log(errorMessage)
+    
+}};
+
+
+async function adddata(uid){
+    try {
+        const docRef = await setDoc(doc(db, "users", uid), {
+            firstName: firstName.value,
+            lastName: lastName.value,
+            email: signEmail.value,
+            password: signPassword.value,
+            phnNumber : phoneNumber.value,
+            dOB : dateTime.value,
+            id : uid     
+        });
+
+        // console.log(firstName,lastName,signEmail,signPassword,dateTime);
+        console.log("Document written with ID: ", docRef.uid);
+    } catch (e) {
+        console.error("Error adding document: ", e);
+    }
+    
+}
+
+signupBtn.addEventListener('click',signUpnHandler);
---
+++ firebaseConfig.js
@@ -0,0 +1,24 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.2.0/firebase-app.js";
+
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, onAuthStateChanged, signOut } from "https://www.gstatic.com/firebasejs/10.2.0/firebase-app.js";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBrhHAyR4l-zyAkuOD2xAIvqg3iemcJLx8",
+    authDomain: "hackathon-324.firebaseapp.com",
+    projectId: "hackathon-324",
+    storageBucket: "hackathon-324.appspot.com",
+    messagingSenderId: "776271736927",
+    appId: "1:776271736927:web:26403a1f52835967a9ebea"
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
+const storage = getStorage(app);
+
+export{
+    app,
+    auth,
+    db,
+    storage
+}
